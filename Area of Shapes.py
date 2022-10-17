455a1aef54f098610efa6521d360e48a87d44588
Aryan Makhija <78488284+aryansm8@users.noreply.github.com>
2022-10-17 13:41:16 +0400

    Add files via upload

---
+++ Area of Shapes.py
@@ -0,0 +1,45 @@
+# Where the user needs to input their choice for the program to calculate the area.
+user_input = input("Would you like to find the area of a Circle, Square, Rectangle or Trapezoid:")
+
+# Fuctions
+def circle (a):
+        area_of_circle = 3.14 * (a ** 2)
+        return (area_of_circle)
+
+def square (a):
+    area_of_square = a ** 2
+    return (area_of_square)
+
+def rectangle (a,b):
+        area_of_rectangle = a * b
+        return (area_of_rectangle)
+
+def trapezoid (a,b,h):
+    area_of_trapezoid = ((a + b)/2) * h 
+    return area_of_trapezoid
+
+# Conditional statements and outputs per outcome.
+if user_input.lower() == "circle":
+    radius = int (input ("Please input the radius of your circle:"))
+    
+    print ("The area of your circle is:", circle (radius) )
+
+if user_input.lower() == "square":
+    length = int (input ("Please input the length of your square:"))
+    
+    print ("The area of your square is:", square (length))
+
+if user_input.lower() == "rectangle":
+    length = int (input ("Please input the length of your rectangle:"))
+    width = int (input ("Please input the width of your rectangle:"))
+    
+    print ("The area of your rectangle is:", rectangle (length, width))
+
+if user_input.lower() == "trapezoid":
+    a_base = int (input ("Please input the first base value of your trapezoid:"))
+    b_base = int (input ("Please input the first base value of your trapezoid:"))
+    height = int (input("Please input the height of your trapezoid"))
+
+    print ("The area of your trapezoid is:", trapezoid (a_base, b_base, height))
+
+# End of Program
